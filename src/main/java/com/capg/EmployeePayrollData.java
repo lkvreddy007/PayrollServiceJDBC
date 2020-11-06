ee6a55e77d99b2526507784cafb6d1a392764567
lkvreddy007 <lkvreddy007@gmail.com>
2020-11-06 19:53:22 +0530

    [Krishna]added-abilityToUpdateMultipleEmployeeToPayrollERDBUsingThreadsSoAsToGetBetterResponse

--- src/main/java/com/capg/EmployeePayrollData.java
+++ src/main/java/com/capg/EmployeePayrollData.java
@@ -38,6 +38,12 @@ public class EmployeePayrollData {
 		this.departmentName = departmentName;
 	}
 
+	public EmployeePayrollData(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
 	@Override
 	public String toString() {
 		return "id: "+id+", name: "+name+" ,salary: "+salary+", startDate: "+startDate;
